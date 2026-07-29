29ac98b79d43fd61c9e2b8354f7063f760cbc243
unknown <corneliusizunna@gmail.com>
2026-07-29 01:48:40 +0100

    initial commit

--- project2.py
+++ project2.py
@@ -5,3 +5,4 @@ age = int(input("What is your age? "))
 city = input ("Which city do you live in? ").title().strip()
 career = input("What is your dream career? ").title().strip()
 print(f"Hello {name}\nYou are {age} years old\nYou live in {city}\nYour dream career is {career}")
+
