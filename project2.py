c673c2df5f8b937bc227ddf39cbd0e8c1737d117
Cornelius Izunna <62110617+Corneldecency@users.noreply.github.com>
2026-07-29 02:32:09 +0100

    Add greeting message at the end of the output

--- project2.py
+++ project2.py
@@ -5,4 +5,5 @@ age = int(input("What is your age? "))
 city = input ("Which city do you live in? ").title().strip()
 career = input("What is your dream career? ").title().strip()
 print(f"Hello {name}\nYou are {age} years old\nYou live in {city}\nYour dream career is {career}")
+print("Have a great day!")
 
